d3759c828fa0e42689f2580280abeda0730073c8
Glenn Kruszewski <glennkruszewski@hotmail.com>
2024-04-01 17:07:41 -0400

    Implement person client repository.

--- TRISTAR.Assessment.Client/People/PersonClientRepository.cs
+++ TRISTAR.Assessment.Client/People/PersonClientRepository.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using TRISTAR.Assessment.Infrastructure;
 
 namespace TRISTAR.Assessment.People
 {
@@ -23,10 +29,11 @@ namespace TRISTAR.Assessment.People
         /// </summary>
         /// <param name="parameters"></param>
         /// <returns></returns>
-        public Task<Person> CreatePerson(EditPersonParameters parameters)
+        public async Task<Person> CreatePerson(EditPersonParameters parameters)
         {
-            // Implementation goes here!
-            throw new NotImplementedException();
+            var response = await _httpClient.PostAsJsonAsync("api/people", parameters);
+
+            return await response.Content.ReadFromJsonAsync<Person>();
         }
 
         /// <summary>
@@ -36,8 +43,7 @@ namespace TRISTAR.Assessment.People
         /// <returns></returns>
         public Task DeletePerson(Guid id)
         {
-            // Implementation goes here!
-            throw new NotImplementedException();
+            return _httpClient.DeleteAsync($"api/people/{id}");
         }
 
         /// <summary>
@@ -46,10 +52,14 @@ namespace TRISTAR.Assessment.People
         /// <param name="id"></param>
         /// <param name="parameters"></param>
         /// <returns></returns>
-        public Task<Person> EditPerson(Guid id, EditPersonParameters parameters)
+        public async Task<Person> EditPerson(Guid id, EditPersonParameters parameters)
         {
-            // Implementation goes here!
-            throw new NotImplementedException();
+            var response = await _httpClient.PatchAsJsonAsync($"api/people/{id}", parameters, new JsonSerializerOptions
+            {
+                Converters = { new PatchParametersFactory() }
+            });
+
+            return await response.Content.ReadFromJsonAsync<Person>();
         }
 
         /// <summary>
@@ -57,10 +67,23 @@ namespace TRISTAR.Assessment.People
         /// </summary>
         /// <param name="parameters"></param>
         /// <returns></returns>
-        public Task<IEnumerable<Person>> GetPeople(QueryPersonParameters parameters)
+        public async Task<IEnumerable<Person>> GetPeople(QueryPersonParameters parameters)
         {
-            // Implementation goes here!
-            throw new NotImplementedException();
+            var queryBuilder = new QueryBuilder();
+
+            void AddQueryParameter(string key, IEnumerable<string> parameterValues)
+            {
+                if (parameterValues?.Any() ?? false)
+                {
+                    queryBuilder.Add(key, parameterValues);
+                }
+            }
+
+            AddQueryParameter(nameof(QueryPersonParameters.FirstName), parameters.FirstName);
+            AddQueryParameter(nameof(QueryPersonParameters.LastName), parameters.LastName);
+            AddQueryParameter(nameof(QueryPersonParameters.Id), parameters.Id?.Select(id => id.ToString()));
+
+            return await _httpClient.GetFromJsonAsync<Person[]>($"api/people/{queryBuilder}");
         }
 
         /// <summary>
@@ -70,8 +93,47 @@ namespace TRISTAR.Assessment.People
         /// <returns></returns>
         public Task<Person> GetPerson(Guid id)
         {
-            // Implementation goes here!
-            throw new NotImplementedException();
+            return _httpClient.GetFromJsonAsync<Person>($"api/people/{id}");
+        }
+
+        /// <summary>
+        /// Converter to ignore writing changes that are not being tracked 
+        /// </summary>
+        /// <remarks>
+        /// Subclass properties that are null from not being changed should be ignored during deserialization
+        /// </remarks>
+        private class PatchParametersFactory : JsonConverterFactory
+        {
+            public override bool CanConvert(Type typeToConvert)
+            {
+                return typeof(PatchParametersBase).IsAssignableFrom(typeToConvert);
+            }
+
+            public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+            {
+                return new PatchParametersBaseConverter();
+            }
+
+            private class PatchParametersBaseConverter : JsonConverter<PatchParametersBase>
+            {
+                public override PatchParametersBase Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) => throw new NotImplementedException();
+
+                public override void Write(Utf8JsonWriter writer, PatchParametersBase value, JsonSerializerOptions options)
+                {
+                    var type = value.GetType();
+
+                    writer.WriteStartObject();
+
+                    foreach (var changedProperty in value.GetChangedProperties())
+                    {
+                        writer.WritePropertyName(changedProperty);
+
+                        JsonSerializer.Serialize(writer, type.GetProperty(changedProperty).GetValue(value), options);
+                    }
+
+                    writer.WriteEndObject();
+                }
+            }
         }
     }
 }
